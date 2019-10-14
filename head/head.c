4dd3b88a1457d22d968e51d1464276e5a5a5cd02
Kristopher Carroll <kjcarroll@alaska.edu>
2019-10-13 19:47:05 -0800

    updated filename handling for flow control

--- head/head.c
+++ head/head.c
@@ -20,28 +20,37 @@ int _atoi(const char *str) {
 }
 
 int main(int argc, const char *argv[]) {
+    const char* filename;
     int NUM_LINES = 10;
     int BUFF_SIZE = 4096;
 
+    // checking for too many arguments
     if(argc > 4) {
         errno = E2BIG;
         char* error_str = strerror(errno);
         write(STDERR_FILENO, error_str, _strlen(error_str));
         return -1;
     }
+
+    // handling arguments
     for (int i = 1; i < argc; i++) {
+        // looking for -n flag
         if(*argv[i] == '-') {
+            // flag found wasn't -n, error out
             if(*(++argv[i]) != 'n') {
                 errno = EINVAL;
                 char* error_str = strerror(errno);
                 write(STDERR_FILENO, error_str, _strlen(error_str));
                 return -1;
             }
+            // -n found, set number of lines
             NUM_LINES = _atoi(argv[i + 1]);
             i++;
         }
+
+        // arg is assumed to be a filename, store it
         else {
-            const char* filename = argv[i];
+            filename = argv[i];
             printf("Filename: %s\n", filename);
         }
     }
