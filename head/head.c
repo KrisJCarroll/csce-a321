19939ea047385694ee7cb1a9a9e8428bb76826a9
Kristopher Carroll <kjcarroll@alaska.edu>
2019-10-13 19:09:27 -0800

    added _atoi implementation

--- head/head.c
+++ head/head.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 int lines = 10;
 int BUFF_SIZE = 4096;
@@ -10,8 +11,21 @@ int _strln(const char *str) {
     return 0;
 }
 
+int _atoi(const char *str) {
+    int res = 0;
+    while (*str) {
+        res = res * 10 + (*str) - '0';
+        ++str;
+    }
+    return res;
+}
+
 int main(int argc, char const *argv[]) {
-    char *msg = "apple";
-    printf("%d\n", _strln(msg));
-    return 0;
+    if(argc > 3) {
+        errno = E2BIG;
+        printf("ERROR: %s\n",strerror(errno));
+    }
+
+    char* int_test = "123456";
+    printf("%d\n", _atoi(int_test));
 }
