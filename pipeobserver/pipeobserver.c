1c36e55461d14398285e608dc937935aa1b751ea
Kristopher Carroll <kjcarroll@alaska.edu>
2019-10-17 15:40:52 -0800

    added execvp() functionality

--- pipeobserver/pipeobserver.c
+++ pipeobserver/pipeobserver.c
@@ -2,6 +2,11 @@
 #include <sys/errno.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define READ_END 0
+#define WRITE_END 1
 
 
 
@@ -85,6 +90,19 @@ int parse_args(int argc, const char* argv[], int *fd) {
     return 0;
 }
 
+void dup_and_exec(int fd, int* pipe, char* cmd[]) {
+    if(dup2(pipe[fd], fd) < 0) {
+        write(STDERR_FILENO, strerror(errno), _strlen(strerror(errno)));
+        write(STDERR_FILENO, "\n", 1);
+        exit(1);
+    }
+
+    close(pipe[0]);
+    close(pipe[1]);
+ 
+    execvp(cmd[0], cmd);
+}
+
 int main(int argc, const char* argv[]) {
 
     int filefd = 0;
@@ -92,7 +110,8 @@ int main(int argc, const char* argv[]) {
     int pipe1[2]; // child1 will connect STDOUT to pipe1[1], child2 will connect STDIN to pipe1[0]
     pid_t pid1;
     pid_t pid2;
-    
+
+    // checking for successful parsing of args
     if (parse_args(argc, argv, &filefd) < 0) {
         errno = EINVAL;
         write(STDERR_FILENO, strerror(errno), _strlen(strerror(errno)));
@@ -106,11 +125,37 @@ int main(int argc, const char* argv[]) {
         return 1;
     }
 
+    // making first child
     if ((pid1 = fork()) < 0) {
         write(STDERR_FILENO, strerror(errno), _strlen(strerror(errno)));
         write(STDERR_FILENO, "\n", 1);
         return 1;
     }
+    // child 1
+    char* cmd1[] = {"ls", 0};
+    if (pid1 == 0) {
+        // replace stdout with write end of pipe
+        dup_and_exec(STDOUT_FILENO, pipe1, cmd1);
+    }
+    waitpid(pid1, NULL, 0);
+    // making second child
+    if ((pid2 = fork()) < 0) {
+        write(STDERR_FILENO, strerror(errno), _strlen(strerror(errno)));
+        write(STDERR_FILENO, "\n", 1);
+        return 1;
+    }
+    // child 2
+    char* cmd2[] = {"wc", 0};
+    if (pid2 == 0) {
+        // replace stdin with read end of pipe
+        dup_and_exec(STDIN_FILENO, pipe1, cmd2);
+    }
+
+
+    // parent process
+    close(pipe1[0]);
+    close(pipe1[1]);
+    wait(NULL);
 
     return 0;
 }
