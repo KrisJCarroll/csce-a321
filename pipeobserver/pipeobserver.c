d892a2875e043259afc903f1d20d541ac8b5c634
Kristopher Carroll <kjcarroll@alaska.edu>
2019-10-17 13:13:29 -0800

    added piped process arg parsing

--- pipeobserver/pipeobserver.c
+++ pipeobserver/pipeobserver.c
@@ -11,13 +11,75 @@ int _strlen(const char *str) {
     return 0;
 }
 
+int _strcmp(const char* first, const char* second) {
+    while ((*first) && (*second)) {
+        if (*first < *second) return -1;
+        if (*first > *second) return 1;
+        first++;
+        second++;
+    }
+    return 0; // they are the same strings
+}
+
 int parse_args(int argc, const char* argv[], int *fd) {
+    int i;
     const char* filename = argv[1];
+    const char* cmd1;
+    const char* cmd2;
+    const char* argv1[5];
+    const char* argv2[5];
+
+    const char* usage = "ERROR: expected usage is ./pipeobserver <filename> [ process1 args ] [ process2 args ]\n";
     if ((*fd = open(filename, O_WRONLY | O_APPEND | O_CREAT)) < 0) {
         write(STDERR_FILENO, strerror(errno), _strlen(strerror(errno)));
         write(STDERR_FILENO, "\n", 1);
         return -1;
     }
+
+    // parsing first executable and associated args
+    if (_strcmp(argv[2], "[") != 0) {
+        errno = EINVAL;
+        write(STDERR_FILENO, usage, _strlen(usage));
+        return -1;
+    }
+    cmd1 = argv[3];
+    argv1[0] = argv[3];
+    i = 4;
+    int count = 1;
+    while (_strcmp(argv[i], "]") != 0) {
+        if (i+1 == argc) {
+            errno = EINVAL;
+            write(STDERR_FILENO, usage, _strlen(usage));
+            return -1;
+        }
+        argv1[count] = argv[i];
+        i++;
+        count++;
+    }
+    argv1[count] = NULL;
+
+    // parsing second executable and associated args
+    if (_strcmp(argv[i++], "[")) {
+        errno = EINVAL;
+        write(STDERR_FILENO, usage, _strlen(usage));
+        return -1;
+    }
+    cmd2 = argv[i++];
+    argv2[0] = cmd2;
+    count = 1;
+    while (_strcmp(argv[i], "]") != 0) {
+        if (i + 1 == argc) {
+            errno = EINVAL;
+            write(STDERR_FILENO, usage, _strlen(usage));
+            return -1;
+        }
+        argv2[count] = argv[i];
+        i++;
+        count++;
+    }
+    argv2[count] = NULL;
+
+
     return 0;
 }
 
