4e08444f795353a15d3daacc7c2d8ccddc46c5cf
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-12 19:14:47 -0900

    added more test cases for calloc()

--- memoryalloc/test.c
+++ memoryalloc/test.c
@@ -8,4 +8,16 @@ int main(int argc, const char *argv[]) {
     free(little_essay);
     free(bigger_essay);
     free(essay);
+
+    char* all_zeroes = calloc(10, sizeof(char));
+    printf("%s",all_zeroes);
+    printf("%s", "\n");
+    
+    char* count_to_nine = calloc(10, sizeof(char));
+    for(int i = 0; i < 10; i++) {
+        count_to_nine[i] = i + '0';
+    }
+    printf("%s", count_to_nine);
+    printf("%s", "\n");
+
 }
