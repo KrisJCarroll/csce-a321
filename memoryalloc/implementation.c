447caaaa07200a18cf1a4e834a7902673119ae97
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-12 21:27:52 -0900

    adjusted pointer arithmetic in free

--- memoryalloc/implementation.c
+++ memoryalloc/implementation.c
@@ -303,8 +303,7 @@ void *__realloc_impl(void *ptr, size_t size) {
 
 void __free_impl(void *ptr) {
   if (ptr == NULL) return;
-  memblock_t* memblock = (memblock_t*) (ptr - HEADER_SIZE);
-  __insert_memblock(memblock);
+  __insert_memblock(ptr - HEADER_SIZE);
 }
 
 /* End of the actual malloc/calloc/realloc/free functions */
