958e2d0fa1cd13b1ef56a4e9b12c89c64411eaaf
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-12 23:56:57 -0900

    adjusted free to prevent seg fault

--- memoryalloc/implementation.c
+++ memoryalloc/implementation.c
@@ -321,7 +321,8 @@ void *__realloc_impl(void *ptr, size_t size) {
 
 void __free_impl(void *ptr) {
   if (ptr == NULL) return;
-  __insert_memblock((memblock_t*)ptr - HEADER_SIZE);
+  memblock_t* freed = (memblock_t*) (ptr - HEADER_SIZE);
+  __insert_memblock(freed);
 }
 
 /* End of the actual malloc/calloc/realloc/free functions */
