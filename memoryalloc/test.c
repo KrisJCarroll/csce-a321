1ee64965c218e4337c8100db3600ed6a33f1d615
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-24 02:36:52 -0900

    updated test cases

--- memoryalloc/test.c
+++ memoryalloc/test.c
@@ -2,29 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, const char *argv[]) {
-    char* essay = malloc(sizeof(char)*64000);
-    char* bigger_essay = malloc(sizeof(char) * 256000);
-    char* little_essay = malloc(sizeof(char) * 32000);
-    free(little_essay);
-    free(bigger_essay);
-    free(essay);
+    char* null = malloc(0);
+    free(null);
+    char* null = calloc(0, 0);
+    free(null);
+    char* null = calloc(0, sizeof(int));
+    free(null);
+    char* null = calloc(100000, 0);
+    free(null);
 
-    char* jiggabyte = malloc(1073741824);
-    free(jiggabyte);
+    char* realbig = malloc(1073741824);
+    free (realbig);
 
-    printf("Getting ready to test calloc...");
-    char* all_zeroes = calloc(10, sizeof(char));
-    printf("Here's all_zeroes: %s",all_zeroes);
-    printf("%s", "\n");
-    
-    char* count_to_nine = calloc(10, sizeof(char));
-    for(int i = 0; i < 10; i++) {
-        count_to_nine[i] = i + '0';
-    }
-    printf("Here's count_to_nine after we changed it: %s", count_to_nine);
-    printf("%s", "\n");
-
-    free(all_zeroes);
-    free(count_to_nine);
+    char* too_big = malloc(1073741824 * 5);
+    free(too_big);
 
 }
